e0af94d54ee8bb50121bc93b1ef90af0fe86c78c
Cupp <146663063+Cupp300@users.noreply.github.com>
2025-10-01 00:07:40 +0200

    Implimented art and Tiles be good

--- Assets/Scripts/Cupp/Game manager.cs
+++ Assets/Scripts/Cupp/Game manager.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class Gamemanager : MonoBehaviour
 {
-    [SerializeField] GameObject player;
+    [Header("UI")]
+    [SerializeField] GameObject UI;
     [SerializeField] GameObject deathScreen;
+    [SerializeField] GameObject mainMenu;
+
+    [SerializeField] GameObject player;
+    
+    private Vector2 respawnPoint;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        respawnPoint = player.transform.position;
+        UI.SetActive(true);
+        deathScreen.SetActive(false);
+        mainMenu.SetActive(false);
     }
 
     // Update is called once per frame
@@ -18,9 +27,15 @@ public class Gamemanager : MonoBehaviour
     {
         
     }
-    void Death()
+    public void Death()
     {
         player.SetActive(false);
-
+        deathScreen.SetActive(true);
+    }
+    public void Respawn()
+    {
+        player.transform.position = respawnPoint;
+        player.SetActive(true);
+        deathScreen.SetActive(false);
     }
 }
