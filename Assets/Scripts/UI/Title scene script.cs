aa2f1e3a1737b4c52a396c3b85b4a188f50d98da
krestenub <114006864+krestenub@users.noreply.github.com>
2025-09-30 10:13:02 +0200

    mf

--- Assets/Scripts/UI/Title scene script.cs
+++ Assets/Scripts/UI/Title scene script.cs
@@ -6,10 +6,11 @@ using UnityEngine.SceneManagement;
 
 public class Titlescenescript : MonoBehaviour
 {
+    public string what_scene;
     // Start is called before the first frame update
     public void skift()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(what_scene);
 
     }
     // Update is called once per frame
