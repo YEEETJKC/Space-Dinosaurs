b47079486e4d37ec0831f09ea89c00c489b68044
krestenub <114006864+krestenub@users.noreply.github.com>
2025-09-30 10:01:32 +0200

    skub

---
+++ Assets/Scripts/UI/Title scene script.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Titlescenescript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void skift()
+    {
+        SceneManager.LoadScene(0);
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+}
