8e7b88f6734c104aa7b277315ee283ae46c08706
Cupp <146663063+Cupp300@users.noreply.github.com>
2025-09-30 14:06:41 +0200

    Tilemaps work

---
+++ Assets/Scripts/Cupp/Game manager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gamemanager : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    [SerializeField] GameObject deathScreen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void Death()
+    {
+        player.SetActive(false);
+
+    }
+}
