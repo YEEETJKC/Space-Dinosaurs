ce6b3fbd83a84f503926b3bed0c7fd4eef65b53c
Cupp <146663063+Cupp300@users.noreply.github.com>
2025-10-01 12:25:05 +0200

    Poison plant better

--- Assets/Slowpoke.cs
+++ Assets/Slowpoke.cs
@@ -7,9 +7,11 @@ public class Slowpoke : MonoBehaviour
 {
     [SerializeField] PlayerController player;
     float speed;
+    float jumpTakeOffSpeed;
     void Start()
     {
         speed = player.maxSpeed;
+        jumpTakeOffSpeed = player.jumpTakeOffSpeed;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -17,10 +19,12 @@ public class Slowpoke : MonoBehaviour
         if (other.tag == "Player")
         {
             player.maxSpeed = speed * 0.5f;
+            player.jumpTakeOffSpeed = jumpTakeOffSpeed * 0.5f;
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
         player.maxSpeed = speed;
+        player.jumpTakeOffSpeed = jumpTakeOffSpeed;
     }
 }
